bb5a8adc779c53e5a94e0219349e7dde2a4b4f67
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-10-06 22:08:35 +0300

    the slider for 2 pictures was created and now slider is separate in component file, also chenged header

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,41 @@
 import React from "react";
 
+import { BsSkype } from "react-icons/bs";
+import {
+  FaRegEnvelope,
+  FaPhoneAlt,
+  FaEnvelope,
+  // FaFacebook,
+  FaPaperPlane,
+} from "react-icons/fa";
+
 const Header = () => {
   return (
     <header className="headInfo">
       <div className="headInfo__logo">
         <img src="../img/logo.bmp" alt="logo" />
       </div>
-      <div className="headInfo__title">
-        <h1>ГипсокартонOK</h1>
-      </div>
-      <div className="headInfo__contacts">
-        <div className="contacts__item tel">
-          <p>(093)333-33-33</p>
+      <div className="head__right">
+        <div className="headInfo__title">
+          <h1>ГипсокартонOK</h1>
         </div>
-        <div className="contacts__item email">
-          <p>gipsok@ukr.net</p>
-        </div>
-        <div className="contacts__item skype">
-          <p>skype_gipsok</p>
-        </div>
-        <div className="contacts__item tel">
-          <p>(093)333-33-33</p>
+        <div className="headInfo__contacts">
+          <div className="contacts__item tel">
+            <FaPhoneAlt />
+            <span> (093)333-33-33</span>
+          </div>
+          <div className="contacts__item email">
+            <FaEnvelope />
+            <span> gipsok@ukr.net</span>
+          </div>
+          <div className="contacts__item skype">
+            <BsSkype />
+            <span> skype_gipsok</span>
+          </div>
+          <div className="contacts__item tel">
+            <FaPaperPlane />
+            <span> (093)333-33-33</span>
+          </div>
         </div>
       </div>
     </header>
--- src/Main.js
+++ src/Main.js
@@ -1,87 +1,6 @@
 import React, { useState, useEffect } from "react";
 import data from "./menu.js";
-
-const Slider = ({ slider }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  if (slider.length === 2) {
-    slider = [...slider, ...slider];
-  }
-
-  useEffect(() => {
-    let lastSlide = slider.length - 1;
-    if (activeIndex < 0) {
-      setActiveIndex(lastSlide);
-    }
-    if (activeIndex > lastSlide) {
-      setActiveIndex(0);
-    }
-  }, [activeIndex, slider, slider.length]);
-  useEffect(() => {
-    let sliderInterval = setInterval(() => {
-      setActiveIndex(activeIndex + 1);
-    }, 3000);
-    return () => clearInterval(sliderInterval);
-  }, [activeIndex]);
-
-  // useEffect(() => {
-  //   setActiveIndex(0);
-  // }, [slider]);
-
-  return (
-    <div className="slider">
-      {slider.map((slide, slideIndex) => {
-        const { title, image, text } = slide;
-        let position = "slider__nextSlide";
-        if (slideIndex === activeIndex) {
-          position = "slider__activeSlide";
-        }
-        if (
-          slideIndex === activeIndex - 1 ||
-          (activeIndex === 0 && slideIndex === slider.length - 1)
-        ) {
-          position = "slider__lastSlide";
-        }
-
-        return (
-          <div key={slideIndex} className={`slider__article ${position}`}>
-            <h3>{title}</h3>
-            <img className="slider__Img" src={image} alt={title} />
-            <p>{text}</p>
-          </div>
-        );
-      })}
-      <button
-        className="slider__buttonL"
-        onClick={() => setActiveIndex(activeIndex - 1)}
-      >
-        prev
-      </button>
-      <button
-        className="slider__buttonR"
-        onClick={() => setActiveIndex(activeIndex + 1)}
-      >
-        next
-      </button>
-
-      <div className="slider__underlineConteiner">
-        {slider.map((slide, index) => {
-          let clName = "slider__underline";
-          if (index === activeIndex) {
-            clName = "slider__underline slider__underline_active";
-          }
-          return (
-            <button
-              key={index}
-              className={clName}
-              onClick={() => setActiveIndex(index)}
-            ></button>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+import Slider from "./Slider";
 
 const Main = () => {
   const [curItem, setCurItem] = useState(0);
@@ -130,18 +49,6 @@ const Main = () => {
             />
             <p>{slider[0].text}</p>
           </article>
-
-          {/* {slider.map((slide, slidIndex) => {
-            const { title, image, text } = slide;
-            let position = "activeSlide";
-            return (
-              <article className={position} key={slidIndex}>
-                <h3>{title}</h3>
-                <img className="sliderImg" src={image} alt={title} />
-                <p>{text}</p>
-              </article>
-            );
-          })} */}
         </section>
       )}
     </main>
